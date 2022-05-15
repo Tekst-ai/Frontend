6e005b1863784a4af49b36461719276fe69fbd85
Jan Deschacht <jandeschacht33@gmail.com>
2022-05-15 22:35:32 +0200

    Start configuration page

--- pages/configuration/index.tsx
+++ pages/configuration/index.tsx
@@ -1,10 +1,24 @@
 import type { NextPage } from 'next'
+import styled from 'styled-components'
+
+import { Colors } from '../../variables'
+
+const TitleContainer = styled.div`
+    p {
+        margin-top: 0.625rem;
+        color: ${Colors.textGrey};
+    }
+`
 
 const Configuration: NextPage = () => {
     return (
-      <div>
-          Configuration
-      </div>
+        <div>
+            <TitleContainer>
+                <h1>Configuration</h1>
+
+                <p>Kies een accent kleur of verander het thema</p>
+            </TitleContainer>
+        </div>
     )
   }
   
