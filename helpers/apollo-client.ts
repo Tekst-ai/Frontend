a8898e7d6981622f4f0a8d317d68d40cfec9f769
Jan Deschacht <jandesch@student.arteveldehs.be>
2022-06-11 19:03:21 +0200

    ENV change

--- helpers/apollo-client.ts
+++ helpers/apollo-client.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { CheckEnv } from '../services/checks';
 
 const client = new ApolloClient({
-    uri: "http://localhost:1337/graphql",
+    uri: CheckEnv(process.env.CMS_ENDPOINT),
     cache: new InMemoryCache(),
 })
 
